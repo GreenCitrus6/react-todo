1fe36a62b1be65fae38ec3788f6f5e67e3d5ff7e
Daniel A <DanielAAad01@gmail.com>
2023-12-05 16:15:51 -0800

    capped width of applet

--- src/app/components/applayout.js
+++ src/app/components/applayout.js
@@ -84,7 +84,7 @@ export default function AppLayout(props) {
     }
 
     return(
-        <main className="border-4 border-gray-600 flex flex-col justify-between rounded-3xl w-5/6 p-5 h-4/5">
+        <main className="border-4 border-gray-600 flex flex-col justify-between rounded-3xl w-5/6 p-5 h-4/5 max-w-6xl">
           <div className="h-5/6">
             <h1 className="font-bold text-3xl text-gray-600">To Do List:</h1>
             <ul id="todoUl" className="mt-3 h-[90%] overflow-y-scroll">
